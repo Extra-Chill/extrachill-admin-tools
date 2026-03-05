991dcc2171b9a8e9b90ccb168ae0f7d0b82b1ecb
Chris Huber <chubes@extrachill.com>
2026-03-05 00:07:34 +0000

    Fix QR ability for Endroid v6 API

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -5,6 +5,11 @@
  The format is based on [Keep a Changelog](https://keepachangelog.com/en/1/0/0/),
  and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Fixed
+- Fix QR ability for Endroid v6 by using constructor API instead of deprecated QrCode::create().
+
 ## [2.1.1] - 2026-03-04
 
 ### Fixed
--- inc/core/abilities.php
+++ inc/core/abilities.php
@@ -539,9 +539,13 @@ function extrachill_admin_tools_ability_generate_qr( $input ) {
 		return new WP_Error( 'dependency_missing', 'Endroid QR Code library not available.' );
 	}
 
-	$qr_code = \Endroid\QrCode\QrCode::create( $input['url'] )
-		->setSize( 1000 )
-		->setMargin( 10 );
+	$qr_code = new \Endroid\QrCode\QrCode(
+		data: $input['url'],
+		encoding: new \Endroid\QrCode\Encoding\Encoding( 'UTF-8' ),
+		errorCorrectionLevel: \Endroid\QrCode\ErrorCorrectionLevel::High,
+		size: 1000,
+		margin: 40
+	);
 
 	$writer = new \Endroid\QrCode\Writer\PngWriter();
 	$result = $writer->write( $qr_code );
